ddb44901bed56e92affaa0cf1766b6001bd58a54
mArchilli <110573064+mArchilli@users.noreply.github.com>
2024-11-27 08:46:09 -0300

    vista userProfile actualizados estilos

--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -5,13 +5,14 @@ import Button from '../components/Button.vue';
 import Skeleton from '../components/Skeleton.vue';
 import LinkUser from '../components/LinkUser.vue';
 import Divider from '../components/Divider.vue';
+import ButtonBack from '../components/ButtonBack.vue';
 import { getUserProfileById } from '../services/user_profile';
 import { getPostsByUser } from '../services/post';
 import { getAuth } from "firebase/auth";
 
 export default {
   name: 'UserProfile',
-  components: { MainH1, MainH2, Button, LinkUser, Skeleton, Divider },
+  components: { MainH1, MainH2, Button, LinkUser, Skeleton, Divider, ButtonBack },
   data() {
     return {
       user: {
@@ -75,16 +76,45 @@ export default {
 <template>
   <div class="p-10">
     <!-- Encabezado de perfil -->
-    <div class="flex items-center justify-between">
-      <div>
-        <MainH1>Perfil de {{ user.email }}</MainH1>
+    <div class="flex flex-row mb-6 items-center">
+      <div class="w-full sm:w-3/4">
+        <MainH1 class="text-center sm:text-left hidden">Mi Perfil</MainH1>
+        <ButtonBack class="text-gray-700 py-5">
+          <router-link to="/posts">Volver</router-link>
+        </ButtonBack>
       </div>
-    </div>
+    </div> 
 
-    <!-- Descripción del usuario -->
-    <div class="my-4">
-      <p>{{ user.nombre }}</p>
-    </div>
+    <section  class="mb-10">
+      <MainH1 class="text-center text-2xl font-semibold mb-6 hidden">Información de mi perfil</MainH1> 
+
+      <div class="flex items-center justify-center space-x-6 p-6 border rounded-lg shadow-md bg-white">
+        
+        <!-- Imagen del usuario -->
+        <div class="w-24 h-24 md:w-32 md:h-32">
+          <img 
+            v-if="user.image_url" 
+            :src="user.image_url" 
+            alt="Foto de perfil" 
+            class="w-full h-full object-cover object-center rounded-full border-4 border-white shadow-lg cursor-pointer"
+            @click="openPreview" 
+          />
+          <div 
+            v-else 
+            class="w-full h-full flex items-center justify-center bg-gray-300 text-white font-semibold rounded-full border-4 border-white shadow-lg">
+            Sin foto
+          </div>
+        </div>
+
+        <!-- Detalles del perfil -->
+        <div class="flex flex-col justify-center">
+          <!-- <p class="text-lg font-medium"><strong>Email:</strong> {{ profile.email }}</p> -->
+          <p v-if="user.nombre" class="text-sm text-black mt-2"><strong>{{ user.nombre }}</strong></p>
+          <p v-if="user.biografia" class="text-sm text-gray-700 mt-2">{{ user.biografia }}</p>
+          <!-- <p v-if="profile.genero" class="text-sm text-gray-700 mt-2"><strong>Género:</strong> {{ profile.genero }}</p> -->
+        </div>
+      </div>
+    </section>
 
     <div class="w-full mb-10">
       <Divider></Divider>
