a1c7ab0e2aa2e2383ff992280c986a9bc39f5a66
mArchilli <110573064+mArchilli@users.noreply.github.com>
2024-12-02 09:38:25 -0300

    configuracion firebase protegida

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApJ-PCfBUUvh9OvdqZ6MFzDLfBfeEcgCQ",
-    authDomain: "libra-link-bb94c.firebaseapp.com",
-    projectId: "libra-link-bb94c",
-    storageBucket: "libra-link-bb94c.appspot.com",
-    messagingSenderId: "60036069257",
-    appId: "1:60036069257:web:d8c0de2f1142baae2c1358"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
   };
 
 // Inicializamos Firebase.
